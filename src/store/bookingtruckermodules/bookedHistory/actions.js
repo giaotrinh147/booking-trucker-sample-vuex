c18f31016d28ea05d6365514347a2ec39a1d833b
Phuong NGUYEN <phuong.nguyen@bollore.com>
2019-10-07 15:37:46 +0700

    update

--- src/store/bookingtruckermodules/bookedHistory/actions.js
+++ src/store/bookingtruckermodules/bookedHistory/actions.js
@@ -1,22 +1,6 @@
 import truckersList from '../../../api/truckers'
 
 export const actions = {
-    FinishBooking ({ commit, state}, truckers) {
-        const bookedTruckers = [...state.items]
-        commit('setFinishBookingStatus', null)
-        // empty booking
-        commit('setBookedTruckers', {items: [] })
-        truckersList.bookTruckers(
-            truckers,
-            () => commit('setFinishBookingStatus', 'successful'),
-            () => {
-                commit('setFinishBookingStatus', 'failed')
-                //rollback to the booked list before sending request
-                commit('setBookedTruckers', { items: bookedTruckers })
-            }
-        )
-    },
-
     addfinishBookingToOders ({ state, commit}, bookedList) {
         commit('pushBookedListToOders', { id: trucker.id})
         }
